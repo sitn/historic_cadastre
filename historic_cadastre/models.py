2b4e49ded41917e4c58c140063cacb08b4db9dba
Michael Kalbermatten <michael.kalbermatten@ne.ch>
2014-01-31 11:30:01 +0100

    Model of first view

--- historic_cadastre/models.py
+++ historic_cadastre/models.py
@@ -13,3 +13,11 @@ from geoalchemy import GeometryColumn, Geometry
     
 Base = sqlahelper.get_base()
 DBSession = sqlahelper.get_session()
+
+print DBSession
+
+class VPlanGraphique(Base):
+    __label__ = 'v_plan_graphique'
+    __tablename__ ='v_plan_graphique'
+    __table_args__ = {'schema': 'plan_histo', 'autoload': True}
+    id_plan = Column('id_plan', Integer, primary_key=True)
