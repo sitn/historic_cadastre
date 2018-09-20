2330173c02bf72203fa8966e32dd77af37d203f1
Pabst Sylvain <PabstS@ne.ch>
2018-09-20 13:07:07 +0200

    historicparcel correction indentation js

--- historic_cadastre/static/js/Plugins/parcel_histo_doc.js
+++ historic_cadastre/static/js/Plugins/parcel_histo_doc.js
@@ -1,28 +1,28 @@
 ﻿$(document).ready(function() {
 
     jQuery.extend( jQuery.fn.dataTableExt.oSort, {
-    "date-euro-pre": function ( a ) {
-        var x;
+        "date-euro-pre": function ( a ) {
+            var x;
 
-        if ( $.trim(a) !== '' ) {
-            var frDatea = $.trim(a).split(' ');
-            var frDatea2 = frDatea[0].split('.');
-            x = (frDatea2[2] + frDatea2[1] + frDatea2[0]) * 1;
+            if ( $.trim(a) !== '' ) {
+                var frDatea = $.trim(a).split(' ');
+                var frDatea2 = frDatea[0].split('.');
+                x = (frDatea2[2] + frDatea2[1] + frDatea2[0]) * 1;
+            }
+            else {
+                x = Infinity;
+            }
+
+            return x;
+        },
+
+        "date-euro-asc": function ( a, b ) {
+            return a - b;
+        },
+
+        "date-euro-desc": function ( a, b ) {
+            return b - a;
         }
-        else {
-            x = Infinity;
-        }
-
-        return x;
-    },
-
-    "date-euro-asc": function ( a, b ) {
-        return a - b;
-    },
-
-    "date-euro-desc": function ( a, b ) {
-        return b - a;
-    }
     });
 
      /* Build the DataTable with third column using our custom sort functions */
